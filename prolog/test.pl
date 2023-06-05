7115811e09feedea34a6bdbdb6007ce047de3f04
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2023-06-05 22:30:59 +0100

    remove unnecessary files

--- prolog/test.pl
+++ prolog/test.pl
@@ -35,8 +35,8 @@ random_value_selection(Var, Rest, BB0, BB1):-
 * get_test_combinations(-Test_Combinations)
 */
 get_test_combinations(Test_Combinations):-
-  Variable_Ordering = [leftmost, min, max, ff, anti_first_fail, occurrence, ffc, max_regret, impact, dom_w_deg, variable(random_variable_ordering)],
-  Value_Selection = [step, enum, bisect, median, middle, value(random_value_selection)],
+  Variable_Ordering = [leftmost, ffc],  % Other values: [min, max, ff, anti_first_fail, occurrence, max_regret, impact, dom_w_deg, variable(random_variable_ordering)],
+  Value_Selection = [step, bisect],     % Other values: [enum , median, middle, value(random_value_selection)],
   Value_Ordering = [up, down],
   Time_Limits = [30000, 60000, 300000, 600000, 1200000], % 30 sec, 1 min, 5 min, 10 min, 20 min
 
@@ -56,17 +56,19 @@ get_test_combinations(Test_Combinations):-
     ), Test_Combinations).
 
 
-print_solution(Solution):-
-  statistics(total_runtime,[_,T]),
-  TS is ((T//10)*10)/1000, nl,
-  write('Labeling Time: '), write(TS), write('s'), nl,
-  Solution = [Routes, Total_Time, Flag],
+print_solution([_, _, time_out]):-
+  write('Flag: '), write(time_out), nl,
+  print_labeling_time.
+print_solution([Routes, Total_Time, Flag]):-
+  print_labeling_time,
   write('Flag: '), write(Flag), nl,
-  print_routes(Routes, Flag),
+  write('Routes: '), write(Routes), nl,
   write('Total Time: '), write(Total_Time), nl.
 
-print_routes(_, time_out):- !.
-print_routes(Routes, _):- write('Routes: '), write(Routes), nl.
+print_labeling_time:-
+  statistics(total_runtime,[_,T]),
+  TS is ((T//10)*10)/1000, nl,
+  write('Labeling Time: '), write(TS), write('s'), nl.
 
 
 print_configuration(Variable_Ordering, Value_Selection, Value_Ordering, Time_Limit):-
@@ -81,12 +83,13 @@ test:-
   (
     foreach([File, Variable_Ordering, Value_Selection, Value_Ordering, Time_Limit], Test_Combinations) do
     solve_problem(File, Variable_Ordering, Value_Selection, Value_Ordering, Time_Limit, Solution),
-    open('./results/test.txt', append, File_Stream),
+    open('./results/test_small.txt', append, File_Stream),
     set_output(File_Stream),
     write(File), write(':'), nl,
     print_solution(Solution),
     print_configuration(Variable_Ordering, Value_Selection, Value_Ordering, Time_Limit),
-    close(File_Stream)
+    close(File_Stream),
+    write(File), nl, write('Time: '), write(Time_Limit), nl, nl
   ).
 
 
